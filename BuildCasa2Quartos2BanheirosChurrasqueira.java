76a5e79569f4181a7f6cbd4e47d01980be6c5f5a
Marcela781 <marcela.andrade@aluno.imepac.edu.br>
2025-04-05 13:18:59 -0300

    Add files via upload

---
+++ BuildCasa2Quartos2BanheirosChurrasqueira.java
@@ -0,0 +1,17 @@
+package br.edu.imepac;
+
+public interface BuildCasa2Quartos2BanheirosChurrasqueira {
+    package br.edu.imepac;
+
+    public interface BuilderCasa2Quartos2banheirosChurrasqueira {
+        public interface BuilderCasa {
+            void buuildQuartos();
+
+            void buildBanheiros();
+
+            void buildChurrasqueira();
+
+            Casa2Quartos2BanheirosChurrasqueira getCasa2Quartos2BanheirosChurrasqueira();
+        }
+    }
+}
---
+++ BuilderCasa2Quartos.java
@@ -0,0 +1,15 @@
+package br.edu.imepac;
+
+    public interface BuilderCasa2Quartos {
+        public interface BuilderCasa {
+            void buuildQuartos();
+
+            void buildBanheiros();
+
+            void buildChurrasqueira();
+
+            Casa2Quartos getCasa2Quartos();
+        }
+    }
+
+
